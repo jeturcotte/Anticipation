ee5e6bc5771c08fe67fd6ba9cc43e25440b4d290
jeturcotte <joshua.eric.turcotte@gmail.com>
2016-09-08 21:56:06 -0400

    still trying to decide HOW to model this stuff

--- generate_models.R
+++ generate_models.R
@@ -1,9 +1,20 @@
-library(quanteda)
-files <- list.files('./data/in', pattern = 'en_US*', full.names = T)
-
-texts <- corpus(readLines('data/in/en_US.blogs.txt',n=100000))
-tokens <- toLower(tokenize( texts, removeNumbers = T, removePunct = T, removeSeparators = T, removeTwitter = T, verbose = T))
 #tokens <- removeFeatures(tokens, stopwords())
-stems <- wordstem(tokens, 'english')
-trigrams <- ngrams(tokens,n=3,concatenator=" ")
-tg_matrix <- dfm(trigrams, verbose = F)  # this makes an incident matrix!
+#stems <- wordstem(tokens, 'english')
+#trigrams <- ngrams(tokens,n=3,concatenator=" ")
+#tg_matrix <- dfm(trigrams, verbose = F)  # this makes an incident matrix!
+
+#setwd("~/R/PROJECTS/Anticipation")
+library(quanteda)
+library(markovchain)
+ptm <- proc.time()
+texts <- corpus(readLines('data/in/en_US.blogs.txt',n=10000))
+proc.time() - ptm
+tokens <- toLower(tokenize( texts, removeNumbers = T, removePunct = T, removeSeparators = T, removeTwitter = T, verbose = T))
+#stems <- wordstem(tokens, 'english')
+ptm <- proc.time()
+fit <- markovchainFit( data=unlist(tokens) )
+proc.time() - ptm
+ptm <- proc.time()
+predict( fit$estimate, newdata=c("where","was","i"), n.ahead=10)
+proc.time() - ptm
+
