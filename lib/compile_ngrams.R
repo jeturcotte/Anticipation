a2c5cffeddd9f76367e8d2f4ef982e20107c8f19
jeturcotte <joshua.eric.turcotte@gmail.com>
2016-09-10 14:02:26 -0400

    so muuuuuch better

--- lib/compile_ngrams.R
+++ lib/compile_ngrams.R
@@ -4,21 +4,42 @@
 #tg_matrix <- dfm(trigrams, verbose = F)  # this makes an incident matrix!
 
 library(quanteda)
+library(stringr)
+library(dplyr)
 
 specify_decimal <- function(x, k) format(round(x, k), nsmall=k)
 
 setwd("~/R/PROJECTS/Anticipation")
 
+stem_the_predictors <- function( ngram ) {
+     tokens <- unlist( tokenize( ngram ) )
+     return(
+          paste(
+               paste(
+                    wordstem(
+                         tokens[1:( length(tokens) - 1 )],
+                         language='english'
+                    ),
+                    collapse=" "
+               ),
+               tail( tokens, n=1 ),
+               sep="|"
+          )
+     )
+}
+
+# load the data
 ptm <- proc.time()
 corpus <- c(
      readLines('data/in/en_US.blogs.txt'),
      readLines('data/in/en_US.news.txt'),
      readLines('data/in/en_US.twitter.txt')
 )
-corpus <- head(sample(corpus), 500000)
+corpus <- head(sample(corpus), 1000000)
 print(proc.time() - ptm)
 print('data files loaded and resampled')
 
+# tokenize the data
 ptm <- proc.time()
 corpus <- toLower(
      tokenize(
@@ -33,65 +54,40 @@ corpus <- toLower(
 print( proc.time() - ptm )
 print('data files tokenized')
 
-ngrams <- list()
-lc <- length(corpus)
+# build out EVERY SINGLE bi, tri, and tetragram in the entire sample corpus
+ptm <- proc.time()
+all_ngrams <- unlist( lapply( head(corpus,n=length(corpus)), function(c){ return( ngrams( c, n=2:4, concatenator=" " ) ) } ) )
+print( proc.time() - ptm )
+print('ngrams assembled')
 
-for ( ln in 1:lc ) {
+# now reduce the predictors to stems (but not the predictED)
+ptm <- proc.time()
+all_ngrams <- lapply( as.list(all_ngrams), stem_the_predictors )
+print( proc.time() - ptm )
+print('stemmed all the predictors')
 
-     unstemmed <- unlist( tokenize( unlist( corpus[ln] ) ) )
-     if (is.null(unstemmed)) {
-          next
-     }
-     stemmed <- wordstem( unstemmed )
-     
-     for ( wn in (length(unstemmed)-1):1 ) {
-          
-          bigram <- paste(
-               unstemmed[wn],
-               unstemmed[(wn+1)],
-               sep="|"
-          )
-          if (is.null(ngrams[[bigram]])) {
-               ngrams[[bigram]] <- 1
-          } else {
-               ngrams[[bigram]] <- ngrams[[bigram]] + 1
-          }
-          if ( (wn-1) >= 1 ) {
-               trigram <- paste(
-                    stemmed[(wn-1)], '_',
-                    stemmed[wn], '|',
-                    unstemmed[(wn+1)],
-                    sep=""
-               )
-               if (is.null(ngrams[[trigram]])) {
-                    ngrams[[trigram]] <- 1
-               } else {
-                    ngrams[[trigram]] <- ngrams[[trigram]] + 1
-               }
-          }
-          if ( (wn-2) >= 1) {
-               tetragram <- paste(
-                    stemmed[(wn-2)], '_',
-                    stemmed[(wn-1)], '_',
-                    stemmed[wn], '|',
-                    unstemmed[(wn+1)],
-                    sep=""
-               )
-               if (is.null(ngrams[[tetragram]])) {
-                    ngrams[[tetragram]] <- 1
-               } else {
-                    ngrams[[tetragram]] <- ngrams[[tetragram]] + 1
-               }
-          }
-     }
-     if ( !ln %% 250 ) {
-          print( paste( 
-               specify_decimal( (100 / lc ) * ln, 2 ),
-               "% done at ",
-               ln,
-               " records decomposed out of ",
-               lc,
-               sep=""
-          ) )
-     }
-}
+# create a data frame to contain all this
+ptm <- proc.time()
+ng_frame <- data.frame( ngram=unlist( all_ngrams ) )
+print(proc.time() - ptm)
+print('initial data frame created')
+
+# split out the columns
+ptm <- proc.time()
+ng_frame <- as.data.frame(str_split_fixed( ng_frame$ngram, "\\|", 2))
+print(proc.time() - ptm)
+print('predictors isolated from observations')
+
+# now sum it all up
+ptm <- proc.time()
+ng_frame <- tally( group_by( ng_frame, V1, V2 ), sort=T )
+colnames(ng_frame) <- c('predictors','observed','frequency')
+ng_frame <- group_by( ng_frame, predictors ) %>% mutate( percent = frequency / sum(frequency) )
+print(proc.time() - ptm)
+print('summations unlocked!')
+
+# and save it out before it disappears, for cthulhu's sake!
+ptm <- proc.time()
+saveRDS('data/out/analyzed_ngrams.rds')
+print(proc.time() - ptm)
+print('saved to file ... and done')
